f5fd5763a00b90722162baef6001bc1dc62771b5
jokedurnez <joke.durnez@gmail.com>
2020-02-21 16:51:22 -0800

    worries for future joke

--- src/test/kotlin/com/openlattice/linking/LinkingScoringTest.java
+++ src/test/kotlin/com/openlattice/linking/LinkingScoringTest.java
@@ -1,4 +1,5 @@
 package com.openlattice.linking;
 
 public class LinkingScoringTest {
+// TODO It should be a unit test in the future to see if the input array of the features results in the correct score, to make sure the training order/features/... map to prod
 }
